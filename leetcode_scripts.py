ba3e28b85ecbe3ea8a05c7c14e828eb7cc33bfc6
JedGStumpf <jedgstumpf@gmail.com>
2022-07-24 11:14:09 -0700

    More Solutions

--- leetcode_scripts.py
+++ leetcode_scripts.py
@@ -518,3 +518,397 @@
 
 # IS = isValid("()[]{[]}")
 # print(IS)
+
+
+# from itertools import permutations
+
+
+# def permute(nums: list[int]) -> list[list[int]]:
+#     return list(permutations(nums))
+
+
+# P = permute([1, 2, 3])
+# print(P)
+
+
+# from itertools import permutations
+
+
+# def nextPermutation(nums: list[int]) -> None:
+#     nums = list(permutations(nums))[1]
+#     print(nums)
+
+
+# NP = nextPermutation([1, 2, 3])
+# print(NP)
+
+
+# def exist(board: list[list[str]], word: str) -> bool:
+#     letters = [x for x in word]
+#     count = 0
+#     second_count = 0
+#     final_str = ""
+
+#     while count < len(board):
+
+#         for letter in board[second_count]:
+#             if letter in letters:
+#                 final_str += letter
+#                 letters.pop(letters.index(letter))
+
+#         count += 1
+#         second_count += 1
+
+#     if "".join(sorted([x for x in word])) in "".join(sorted([x for x in final_str])):
+#         return True
+#     return False
+
+
+# E = exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED")
+
+# print(E)
+
+
+# def searchRange(nums: list[int], target: int) -> list[int]:
+
+#     if not nums:
+#         return [-1, -1]
+#     if target not in nums:
+#         return [-1, -1]
+
+#     ind_list = []
+
+#     for index, value in enumerate(nums):
+#         if value == target:
+#             ind_list.append(index)
+
+#     if len(ind_list) == 1:
+#         ind_list.append(ind_list[0])
+
+#     return []
+
+
+# SR = searchRange([5, 7, 7, 8, 8, 10], 8)
+# print(SR)
+
+
+# def searchInsert(nums: list[int], target: int) -> int:
+
+#     if target not in nums:
+
+#         nums.append(target)
+#         nums = sorted(nums)
+#     return nums.index(target)
+
+
+# SI = searchInsert([1, 3, 5, 6], 7)
+# print(SI)
+
+
+# def divide(dividend: int, divisor: int) -> int:
+#     count = 0
+#     subtract_from = dividend
+
+#     if dividend == 0:
+#         return 0
+
+#     if dividend > 0:
+
+#         if divisor > 0:
+#             if divisor == 1:
+#                 count = dividend
+#             else:
+#                 while subtract_from > 0:
+#                     print(f"Sub: {subtract_from}, Count {count}")
+#                     if subtract_from >= divisor:
+#                         subtract_from -= divisor
+#                         count += 1
+#                     else:
+#                         break
+
+#         else:
+#             if divisor == -1:
+#                 count = dividend - (dividend + dividend)
+#             else:
+#                 while subtract_from > 0:
+#                     print(f"Sub: {subtract_from}, Count {count}")
+#                     if (subtract_from + divisor) >= 0:
+#                         count -= 1
+#                         subtract_from += divisor
+#                     else:
+#                         break
+
+#     else:
+
+#         if divisor > 0:  # -dividend/+divisor
+#             if divisor == 1:
+#                 count = dividend
+#             else:
+#                 while subtract_from < 0:
+#                     print(f"Sub: {subtract_from}, Count {count}")
+#                     if (subtract_from + divisor) <= 0:
+#                         subtract_from += divisor
+#                         count -= 1
+#                     else:
+#                         break
+
+#         else:  # -dividend/-divisor
+#             if divisor == -1:
+#                 count = dividend - (dividend + dividend)
+#             else:
+#                 while subtract_from < 0:
+#                     print(f"Sub: {subtract_from}, Count {count}")
+#                     if (subtract_from - divisor) <= 0:
+#                         count += 1
+#                         subtract_from -= divisor
+#                     else:
+#                         break
+
+#     if count > 2147483647:
+#         return 2147483647
+#     if count < -2147483648:
+#         return -2147483648
+
+#     return count
+
+
+# DI = divide(1, -1)
+# print(DI)
+
+
+# def lengthOfLastWord(s: str) -> int:
+#     s = s.strip(" ")
+#     print(s)
+#     s_list = s.split(" ")
+#     print(s_list)
+#     return len(s_list[-1])
+
+
+# LOSW = lengthOfLastWord("   fly me   to   the moon  ")
+# print(LOSW)
+
+
+# from collections import Counter
+
+
+# def majorityElement(nums: list[int]) -> int:
+#     nums_counts = dict(Counter(nums))
+#     print(nums_counts)
+#     nums_keys = list(nums_counts.keys())
+#     nums_values = list(nums_counts.values())
+#     majority_index = nums_values.index(max(nums_values))
+#     print(f"KEYS: {nums_keys}\n Values: {nums_values}\n Majority: {majority_index}")
+#     return nums_keys[majority_index]
+
+
+# ME = majorityElement([3, 3, 4])
+# print(ME)
+
+
+# def largestNumber(nums: list[int]) -> str:
+#     nums_str = [str(x) for x in nums]
+#     nums_concat_max = sorted(nums_str, reverse=True, key=max)
+#     nums_concat = sorted(nums_str, reverse=True)
+#     if int("".join(nums_concat_max)) > int("".join(nums_concat)):
+#         return "".join(nums_concat_max)
+#     else:
+#         return "".join(nums_concat)
+
+
+# LM = largestNumber([432, 43243])
+# print(LM)
+
+# import re
+
+
+# def calculate(s: str) -> int:
+#     s = s.strip(" ")
+#     # s = s.replace("(", "")
+#     # s = s.replace(")", "")
+#     res = sum(map(int, re.findall(r"[+-]?\d+", s)))
+
+#     return res
+
+
+# C = calculate("1-(5)")
+# print(C)
+
+
+# def removeElement(nums: list[int], val: int) -> int:
+#     count = 0
+#     final_list = []
+#     for num in nums:
+#         if num == val:
+#             count += 1
+#         else:
+#             final_list.append(num)
+#     x = ["_"] * (len(nums) - len(final_list))
+#     final_list.extend(x)
+
+#     return count
+
+
+# RE = removeElement([3, 2, 2, 3], 3)
+# print(RE)
+
+# from collections import Counter
+
+
+# def singleNumber(nums: list[int]) -> int:
+#     c = Counter(nums)
+#     print(c)
+
+#     y = [x for x in c.values()]
+#     print(y)
+#     z = [x for x in c.keys()]
+#     print("z ", z)
+#     z_index = y.index(min(y))
+#     print(z_index)
+#     print(z[z_index])
+#     return z[z_index]
+
+
+# SM = singleNumber([4, 1, 2, 1, 2])
+# print(SM)
+
+
+# def findTheDifference(s: str, t: str) -> str:
+#     s_list = set([x for x in s])
+#     t_list = set([x for x in t])
+#     if s_list == t_list:
+#         return list(s_list)[0]
+#     if len(s_list) > len(t_list):
+#         final = list(s_list - t_list)
+#         return final[0]
+#     else:
+#         final = list(t_list - s_list)
+#         return final[0]
+
+
+# FTD = findTheDifference("a", t="aa")
+# print(FTD)
+
+
+# def containsDuplicate(nums: list[int]) -> bool:
+#     if len(nums) > len(set(nums)):
+#         return True
+#     return False
+
+
+# def thirdMax(nums: list[int]) -> int:
+#     nums_sort = sorted(list(set(nums)), reverse=True)
+
+#     if len(nums_sort) < 3:
+#         return nums_sort[0]
+#     return nums_sort[2]
+
+
+# TM = thirdMax([2, 3, 1])
+# print(TM)
+
+
+# def findKthLargest(nums: list[int], k: int) -> int:
+#     nums_sort = sorted(list(set(nums)))
+#     print(nums_sort)
+#     return nums_sort[(-1 * k)]
+
+
+# FKL = findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+# print(FKL)
+
+# from itertools import combinations
+
+# import re
+
+
+# def countVowels(word: str) -> int:
+
+#     return len(
+#         re.findall(
+#             "[aeiou]",
+#             "".join(
+#                 [
+#                     word[i:j]
+#                     for i in range(len(word))
+#                     for j in range(i + 1, len(word) + 1)
+#                 ]
+#             ),
+#         )
+#     )
+
+
+# CV = countVowels(
+#     "vurgbbaivajgfeoqziufeetkjmkdefmpatepbqtiupfwxselhrzvirkfvmriwdfpxkbjpyvdgupnvvaiqglxsvelcqkrfucazluzfxbehnylswzoixftmmbubogscedrjuxaqirxamsgnsbckiebwuhtnvpopfrbqviqmnskuxoqhjsrlanymaovqvuzyjhhtqxudakabiyqofnbgrnfmcwpcvamlkskvfysbttjpcjypvwvepxlannyr"
+# )
+# print(CV)
+
+
+# def mySqrt(x: int) -> int:
+#     count = 0
+#     sub_from = 1
+#     while (x - sub_from) >= 0:
+#         x -= sub_from
+#         count += 1
+#         sub_from += 2
+#     return count
+
+
+# MS = mySqrt(81)
+# print(MS)
+
+
+# from math import sqrt, floor
+
+
+# def isPerfectSquare(num: int) -> bool:
+#     x = sqrt(num)
+#     if floor(x) == x:
+#         return True
+#     return False
+
+
+# IPS = isPerfectSquare(7)
+# print(IPS)
+# from itertools import combinations_with_replacement
+# from math import sqrt, floor
+
+
+# def judgeSquareSum(c: int) -> bool:
+
+#     if c == 0:
+#         return True
+
+#     square_max = floor(sqrt(c))
+
+#     comb_numbers = [
+#         (x ** 2) + (y ** 2)
+#         for x, y in list(combinations_with_replacement(range(square_max + 1), 2))
+#         if (x ** 2) + (y ** 2) == c
+#     ]
+
+#     if c in comb_numbers:
+#         return True
+#     return False
+
+# for group in comb_numbers:
+#     if ((group[0] ** 2) + (group[1] ** 2)) == c:
+#         return True
+# return False
+
+# a = c
+# b = 0
+
+# while True:
+#     if (a ** 2 + b ** 2) == c:
+#         return True
+#     a -= 1
+#     if a == 0:
+#         b += 1
+#         a = c
+
+#     if b == c:
+#         return False
+
+
+# JSS = judgeSquareSum(4)
+# print(JSS)
